aecf0ec033113936ea94525587aada611a75f498
Avinash Gupta <104323130+AvinashFullstackDeveloper@users.noreply.github.com>
2025-01-20 01:26:59 +0530

    products api has been created and tested on postman app.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+import Product from './models/product.model.js'
 
-const app = express();
 dotenv.config();
 
-app.get("/", (req,res) => {
-    res.send("Server is ready !");
-})
+const app = express();
+
+app.use(express.json()); //allows us to accept JSON data in the req.body
+
+app.post("/api/products", async (req,res) => {
+    const product= req.body; //user will send htis data
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({success: false, message: "Please provide al fields"});
+    }
+
+    const newProduct = new Product(product);
+
+    try{
+        await newProduct.save();
+        res.status(201).json({success: true, data:newProduct});
+    } catch(error){
+        console.error("Error in Create product:", error.message);
+        res.status(500).json({success: false,message: "Server Error"});
+    }
+});
 
 console.log(process.env.MONGO_URI)
 
@@ -15,4 +34,7 @@ console.log(process.env.MONGO_URI)
 // });
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT} Avinash`));
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server running on port ${PORT} Avinash`)
+});

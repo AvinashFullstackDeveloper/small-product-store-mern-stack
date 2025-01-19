4d3bcf70725200558cfceed6cf2fa1c974863687
Avinash Gupta <104323130+AvinashFullstackDeveloper@users.noreply.github.com>
2025-01-19 23:51:51 +0530

    MONGO _URI has been consumed.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req,res) => {
     res.send("Server is ready !");
 })
+
+console.log(process.env.MONGO_URI)
 
 // app.listen(5000, ()=> {
 //     console.log("Server started at http://localhost:5000 avinash");
@@ -12,5 +16,3 @@ app.get("/", (req,res) => {
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} Avinash`));
-
-// LQzRgn2KHr81Q8RN

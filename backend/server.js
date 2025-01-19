0cef1355e729c01bc4f47a58602b31a9529cfb9b
Avinash Gupta <104323130+AvinashFullstackDeveloper@users.noreply.github.com>
2025-01-19 23:16:48 +0530

    Server ready setup has been done.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.listen(5000, ()=> {
-    console.log("Server started at http://localhost:5000");
-});
+app.get("/", (req,res) => {
+    res.send("Server is ready !");
+})
+
+// app.listen(5000, ()=> {
+//     console.log("Server started at http://localhost:5000 avinash");
+// });
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT} Avinash`));

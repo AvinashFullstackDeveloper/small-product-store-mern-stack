7b377a676d4854f6fb511c3824a816ad98273c26
Avinash Gupta <104323130+AvinashFullstackDeveloper@users.noreply.github.com>
2025-01-21 00:59:25 +0530

    post method controller has been created.

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,30 +1,14 @@
 import express from "express";
 import mongoose from 'mongoose';
 import Product from '../models/product.model.js';
-import { getProducts } from "../controllers/product.controller.js";
+import { createProduct, getProducts } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 export default router;
 
 router.get("/", getProducts)
-router.post("/", async (req,res) => {
-    const product= req.body; //user will send htis data
-
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({success: false, message: "Please provide al fields"});
-    }
-
-    const newProduct = new Product(product);
-
-    try{
-        await newProduct.save();
-        res.status(201).json({success: true, data:newProduct});
-    } catch(error){
-        console.error("Error in Create product:", error.message);
-        res.status(500).json({success: false,message: "Server Error"});
-    }
-});
+router.post("/", createProduct);
 
 router.put("/:id", async (req,res) => {
     const {id}= req.params;

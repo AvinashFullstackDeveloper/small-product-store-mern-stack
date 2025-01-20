5663cb337fd386ea5d8f5047900707be453023f7
Avinash Gupta <104323130+AvinashFullstackDeveloper@users.noreply.github.com>
2025-01-21 03:59:11 +0530

    Fontend initial setup done and installed Chakra UI.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ChakraProvider, Box } from '@chakra-ui/react';
+import {Button} from "@chakra-ui/react"; 
+import {Rote, Routes} from "react-router-dom";
+
+function App() {
+
+  return (
+    <Box minH={"100vh"}>
+      {/* <NavBar /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cerate" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  )
+}
+
+export default App;

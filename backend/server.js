391d1a450ad22176d8dc628de4fc5ff8871d50a6
Avinash Gupta <104323130+AvinashFullstackDeveloper@users.noreply.github.com>
2025-01-21 01:26:32 +0530

    port number issue has been resolved.

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,13 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5003;
 
 app.use(express.json()); //allows us to accept JSON data in the req.body
 
 app.use("/api/products",productRoutes);
 
-app.listen(PORT, () => {
+app.listen(5004, () => {
     connectDB();
     console.log(`Server running on port ${PORT} Avinash`)
 });

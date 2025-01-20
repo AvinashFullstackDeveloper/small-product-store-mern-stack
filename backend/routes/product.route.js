0c68e37104e313b287435d3fb517aef607275021
Avinash Gupta <104323130+AvinashFullstackDeveloper@users.noreply.github.com>
2025-01-21 01:07:44 +0530

    create and udpate controllers have been implemented.

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from 'mongoose';
 import Product from '../models/product.model.js';
-import { createProduct, getProducts } from "../controllers/product.controller.js";
+import { createProduct, getProducts, updateProduct, deleteProduct } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
@@ -10,34 +10,7 @@ export default router;
 router.get("/", getProducts)
 router.post("/", createProduct);
 
-router.put("/:id", async (req,res) => {
-    const {id}= req.params;
-    const product = req.body;
-
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({success:false,message:"Invalid Product Id"});
-    }
-
-    try{
-        const updatedProduct= await Product.findByIdAndUpdate(id,product,{new:true});
-        res.status(200).json({success:true, data:updatedProduct});
-    }catch(error){
-        res.status(500).json({success:false, message:"Server Error !!"});
-    }
-})
+router.put("/:id", updateProduct);
 
 
-// app.listen(5000, ()=> {
-//     console.log("Server started at http://localhost:5000 avinash");
-// });
-
-router.delete("/:id", async (req,res) => {
-    const {id} =req.params;
-
-    try{
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success:true, message: "Product Deleted !!"});
-    }catch(error){
-        res.status(404).json({success:false, message: "Product not found !!"})
-    }
-});
+router.delete("/:id", deleteProduct);
